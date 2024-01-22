bfb27c606b907c02d2be83810d2ab9c909a969cf
FeNiiX <cronosfelixmail@gmail.com>
2024-01-22 17:08:16 -0300

    Update ytdlp.bat

--- ytdlp.bat
+++ ytdlp.bat
@@ -1,27 +1,45 @@
 @echo off
-:start
+set sp=OFF
 title yt-dlp
+:start
+if not defined link (
 set /p link=Link:
+) else (
+echo Link:%link%
+)
+echo.
 echo ------------------------
 echo Quick Downloads
 echo ------------------------
 echo.
 echo [1] Format 22/18
 echo [2] Best MP4
-echo [3] Default
-echo [4] Audio
-echo [5] Custom
+echo [3] Audio (M4A)
+echo [4] Default
+echo [5] Choose
+echo [6] Sponsorblock %sp%
 echo [0] Cancel
 echo.
 echo ------------------------
-set /p format=Format:
+choice /c 1234560 /n /m "Enter your choice: "
 echo.
-if %format% equ 1 set frmt=22/18
-if %format% equ 2 set frmt=bv*[ext=mp4]+ba[ext=m4a]/b[ext=mp4]
-if %format% equ 3 set frmt=bv*+ba/b
-if %format% equ 4 set frmt=ba[ext=m4a]
-if %format% equ 5 set frmt=-
-if %format% equ 0 goto start
-yt-dlp --embed-metadata --embed-thumbnail -f %frmt% %link%
+if %errorlevel% == 1 set format=-f 22/18 
+if %errorlevel% == 2 set format=-f bv[ext=mp4]+ba[ext=m4a]/b[ext=mp4] 
+if %errorlevel% == 3 set format=-f ba[ext=m4a] 
+if %errorlevel% == 4 set format=
+if %errorlevel% == 5 set format=-f - 
+if %errorlevel% == 6 (
+	if %sp% == OFF (
+		set spbl=--sponsorblock-remove default 
+		set sp=ON
+	) else (
+		set spbl=
+		set sp=OFF
+	)
+	cls
+	goto start
+)
+if %errorlevel% == 7 goto start
+yt-dlp --embed-metadata --embed-thumbnail %spbl%%format%%link%
 echo.
 goto start

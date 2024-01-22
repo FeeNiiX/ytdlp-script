6df8024491de9888cd105364287cc40ff7bc1af3
FeNiiX <cronosfelixmail@gmail.com>
2024-01-22 15:17:22 -0300

    Update

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
 # ytdlp-script
- my beautiful yt-dlp .bat script to quickly download videos
+
+You need [yt-dlp](https://github.com/yt-dlp/yt-dlp)
+
+and [ffmpeg](https://github.com/BtbN/FFmpeg-Builds/releases/tag/latest)
+
+You can make a directory to put Download.bat, yt.dlp.exe and ffmpeg.exe
+
+Or you can set it as a system variable
+
+### How
+
+its in portuguese but you should figure it out where all of this is
+
+since you are smart enough to know what yt-dlp is.
+
+<img src=tutorial\step1.png/>
+
+<img src=tutorial\step2.png/>
+
+<img src=tutorial\step3.png/>
+
+<img src=tutorial\step4.png/>
---
+++ ytdlp.bat
@@ -0,0 +1,27 @@
+@echo off
+:start
+title yt-dlp
+set /p link=Link:
+echo ------------------------
+echo Quick Downloads
+echo ------------------------
+echo.
+echo [1] Format 22/18       |
+echo [2] Best MP4           |
+echo [3] Default            |
+echo [4] Audio              |
+echo [5] Custom             |
+echo [0] Cancel             |
+echo.
+echo ------------------------
+set /p format=Choice:
+echo.
+if %format% == 1 (set format=-f 22/18)
+if %format% == 2 (set format=-f bv[ext=mp4]+ba[ext=m4a]/b[ext=mp4])
+if %format% == 3 (set format=)
+if %format% == 4 (set format=-f ba[ext=m4a])
+if %format% == 5 (set format=-f -)
+if %format% == 0 (goto start)
+yt-dlp --embed-metadata --embed-thumbnail %sponsor% %format% %link%
+echo.
+goto start

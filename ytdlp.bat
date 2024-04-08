87c5cf3bce4dba45ba414f8b0e7bfa28ff152746
FeNiiX <cronosfelixmail@gmail.com>
2024-04-08 19:09:22 -0300

    Update ytdlp.bat

--- ytdlp.bat
+++ ytdlp.bat
@@ -2,17 +2,13 @@
 set sp=OFF
 title yt-dlp
 :start
-if not defined link (
 set /p link=Link:
-) else (
-echo Link:%link%
-)
 echo.
 echo ------------------------
-echo Quick Downloads
+echo Downloads Options
 echo ------------------------
 echo.
-echo [1] Format 22/18
+echo [1] Quick Download
 echo [2] Best MP4
 echo [3] Audio (M4A)
 echo [4] Default
@@ -21,13 +17,14 @@ echo [6] Sponsorblock %sp%
 echo [0] Cancel
 echo.
 echo ------------------------
+echo.
 choice /c 1234560 /n /m "Enter your choice: "
 echo.
-if %errorlevel% == 1 set format=-f 22/18 
-if %errorlevel% == 2 set format=-f bv[ext=mp4]+ba[ext=m4a]/b[ext=mp4] 
-if %errorlevel% == 3 set format=-f ba[ext=m4a] 
+if %errorlevel% == 1 set format=-f 22/18
+if %errorlevel% == 2 set format=-f bv[ext=mp4]+ba[ext=m4a]/b[ext=mp4]
+if %errorlevel% == 3 set format=-f ba[ext=m4a]
 if %errorlevel% == 4 set format=
-if %errorlevel% == 5 set format=-f - 
+if %errorlevel% == 5 set format=-f -
 if %errorlevel% == 6 (
 	if %sp% == OFF (
 		set spbl=--sponsorblock-remove default 
@@ -40,7 +37,6 @@ if %errorlevel% == 6 (
 	goto start
 )
 if %errorlevel% == 7 goto start
-yt-dlp --embed-metadata --embed-thumbnail %spbl%%format%%link%
+yt-dlp --embed-metadata --embed-thumbnail %spbl% %format% %link%
 echo.
-set link=
 goto start
